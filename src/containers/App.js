800d24e305827e6589105311eeb52361582bfedc
= <=>
2018-07-12 20:57:43 -0400

    making redux works well on app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css'
+
+import { setSearchField } from '../actions'
+
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispach) => {
+    return {
+        onSearchChange: (event) => dispach(setSearchField(event.target.value))
+    }
+}
+
 
 class App extends Component {
 
@@ -11,7 +28,6 @@ class App extends Component {
         super()
         this.state = {
             robots: [],
-            searchField: ''
         }
     }
 
@@ -21,15 +37,11 @@ class App extends Component {
             .then(users => this.setState({ robots: users }))
     }
 
-    onSearchChange = (event) => {
-        this.setState({
-            searchField: event.target.value
-        })
-    }
 
     render() {
 
-        const { robots, searchField } = this.state
+        const { robots } = this.state
+        const { searchField, onSearchChange } = this.props;
 
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase())
@@ -41,7 +53,7 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
 
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
                             <CardList robots={filteredRobots} />
@@ -53,4 +65,4 @@ class App extends Component {
 
 }
 
-export default App
+export default connect(mapStateToProps, mapDispatchToProps)(App)
